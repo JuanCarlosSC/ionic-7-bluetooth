f573a49b30e8115e16e1a538f0055645fca37573
Juan Carlos Sanchez Cordova <carlos300891@gmail.com>
2023-06-12 19:22:42 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-bluetooth-6',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
